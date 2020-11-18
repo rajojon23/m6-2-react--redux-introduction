1c9261cc78a5a350c84e78b7f33f2c7bacfba075
rajojon23 <rajojon23@hotmail.com>
2020-11-17 19:51:31 -0500

    adding improved cart look

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,8 +21,8 @@ const Cart = () => {
   return (
     <Wrapper>
 
-        <div>Your Cart</div>
-        <div>{storeItems.length} Items</div>
+        <div className="cart_title">Your Cart</div>
+        <div className="items_number">{storeItems.length} Items</div>
         {
             
             storeItems.map((item) => (
@@ -31,9 +31,10 @@ const Cart = () => {
 
 
         }
-
-        <div>Total: {total}$</div>               
-        <div><button>Purchase</button></div>     
+        <TotalPurchase>
+            <div class="">Total: <span>${total}</span></div>               
+            <button>Purchase</button> 
+        </TotalPurchase>    
         
     </Wrapper>
   );
@@ -45,6 +46,41 @@ const Wrapper = styled.div`
     width: 245px;
     background-color: rgb(64, 31, 67);
     height: 100%;
+
+    .items_number{
+        margin-bottom: 25px;
+        margin-left: 20px;
+        color: lightgray;
+    }
+
+    .cart_title{
+        margin-top: 25px;
+        margin-left: 20px;
+        font-size: 23px;
+        font-weight: bold;
+    }
+`;
+
+const TotalPurchase = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 25px;
+    margin-left: 20px;
+    justify-content: space-around;
+    align-items: center;
+
+    span{
+        font-weight: bold;
+    }
+
+    button{
+        border-radius: 12px;
+        background: #ff406e;
+        color: white;
+        border: none;
+        padding: 8px;
+        font-weight: bold;
+    }
 `;
 
 
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -11,15 +11,18 @@ const CartItem = ({id, title, src, price}) => {
     console.log("item", {id, title, price});
     return (
         <Wrapper>
-            <div><button onClick={() =>
+            
+            <ItemContainer>
+            <TitleRemove>
+                <div>{title}</div> 
+                <button className="remove" onClick={() =>
 
                 dispatch(removeItem({id, title, price}))
                 // console.log("inside onclick")
-            }> Remove</button></div>
-
-            <div>{title}</div>               
-            <div>quantity: <input value="1"></input> </div>               
-
+                }> X </button>
+            </TitleRemove>        
+                <div className="quantity"><span>Quantity:</span> <input value="1"></input> </div>               
+            </ItemContainer>
 
         </Wrapper>
     );
@@ -35,6 +38,48 @@ const Wrapper = styled.div`
   div{
     margin: 5px;
   }
+
+  .remove{
+      color: #fff;
+      cursor: pointer;
+      border: none;
+      background: none;
+      margin-left: auto; //needed to put on the far right as a flex child
+      
+  }
+
+  input{
+    width: 19px;
+    border: none;
+    background-color: darkmagenta;
+    color: #fff;
+    border-bottom: 2px solid #fff;
+    text-align: center;
+  }
+  .quantity{
+    background-color: darkmagenta;
+    padding: 6px;
+  }
+
+  .quantity span{
+    color: lightgray;
+  }
 `;
+
+const TitleRemove = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ItemContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 235px;
+    background-color: rgb(64, 31, 67);
+    height: 100%;
+    border: 2px dashed #fff;
+`;
+
+
 
 export default CartItem;  
